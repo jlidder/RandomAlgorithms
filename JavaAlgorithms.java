77dd683dda5ab1c4ea39da30e77ac7b4414a703d
Jaspreet Singh Lidder <jaspreetsinghlidder@gmail.com>
2013-01-19 03:02:08 -0500

    got rid of more redundant code or access calls.

--- JavaAlgorithms.java
+++ JavaAlgorithms.java
@@ -7,20 +7,14 @@ public class JavaAlgorithms
 	 * INPUT: Accepts an ArrayList of positive integers.
 	 * OUTPUT: Returns the value that is duplicated in the input list.
 	 *         Returns -1 if no duplicate is found.
-	 * NOTE: Performance is better than O(n^2).
+	 * NOTE: Performance is better than O(n^2). I think this is O(nLogn)?
 	 */
 	public static int ReturnDuplicate(ArrayList<Integer> list)
 	{
 		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-		
 		for(int memberOfList : list)
-		{
-			if(hm.get(memberOfList)==null)
-				hm.put(memberOfList, memberOfList);
-				
-			else // first sign of a duplicate value.
+			if(hm.put(memberOfList, memberOfList) != null) // if put returns null, then it means there was no previous definition for the specific hash
 				return memberOfList;
-		}
 		return -1;
 	}
 }
